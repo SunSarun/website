51241100004c588d20d972283f78be89659135ad
sunsarun <sunsarun@protonmail.com>
2026-06-25 17:20:30 +0000

    trying to using one build script

--- vite.config.github.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react' 
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/website/',          // 1. Must start (and preferably end) with a slash
-})
